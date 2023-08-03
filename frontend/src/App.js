0f09c3c719bc1adc92d00b867013da760aadfa9f
shreshthkr <shreshthkumar6@gmail.com>
2023-08-04 02:55:24 +0530

    added tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+     <div className='w-35 flex justify-center items-start h-screen'>
+      <img src="https://play-lh.googleusercontent.com/Ldqdy8XStmHTCtjTmvn4Dt8GrU6I55A0fFw6H5l2Au_iXn2d-ZOkJGk3wMcWN1ztXQ" className="w-28 mt-20 flex-start mx-auto rounded-full" alt="form builder" />
+     </div>
   );
 }
 
